3ba059178f1a3e0a8d4258636d5097df8cfc829f
一只小猿猿 <ryh15234883064@foxmail.com>
2022-10-26 18:33:51 +0800

    基础搭建

---
+++ themes/butterfly/source/html/about/assets/js/user.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+	let name = $.cookie('mm-username')
+	let avatar = $.cookie('mm-avatar')
+	if(name != undefined && avatar != undefined){
+		$("#login-box").hide()
+		$("#about-me-box").show()
+		$("#my-name").text(name)
+		$("#user-avatar-1").attr("src",avatar)
+		$("#user-avatar-2").attr("src",avatar)
+	}else{
+		$("#logout-box").hide()
+		$("#about-me-box").hide()
+	}
+	
+	
+})
+
+$(function(){
+	
+	$("#logout-box").click(function(){
+		$.ajax({
+		    url: "/users/logout",
+		    type: "POST",
+		    dataType: "JSON",
+		    success: function(json) {
+		        if(json.state == 200){
+					if($.removeCookie('mm-username', { path: '/' }) && $.removeCookie('mm-avatar', { path: '/' })){
+						location.href = "/users/logout.html"
+					}			
+		
+				} else{
+					alert("退出时发生未知错误")
+				}
+		    },
+		    error: function(xhr) {
+		        alert("登陆时发生未知异常" + xhr.message)
+		    }
+		});
+	
+	})
+})
+
+
+
+
